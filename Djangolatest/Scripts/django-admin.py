3dc04e195bf9fdff7ea2fda3704f24a118c49f67
kelechi <egbosikelechi@gmail.com>
2018-02-20 02:34:26 -0800

    remove untracked files

--- Djangolatest/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\KELECHI\PycharmProjects\Compunetcrm\Djangolatest\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
