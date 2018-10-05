82142746d9613143d427dc9eb020e9d1b5a14ba7
KELECHI <egbosikelechi@gmail.com>
2018-10-05 05:06:52 +0100

    security patch

--- compunetcrm/forms/SendSmsForm.py
+++ compunetcrm/forms/SendSmsForm.py
@@ -3,6 +3,5 @@ from phonenumber_field.formfields import PhoneNumberField
 
 
 class ComposeSmsForm(forms.Form):
-    phone_numbers = PhoneNumberField()
-    phone_number = forms.CharField(max_length=14, label="Phone Numbers",help_text= "Enter Phone Numbers Seperated by ' , ' ")
-    message_body = forms.CharField(max_length=300, label="Compose Message", widget=forms.Textarea, help_text= "Enter Approved Official Email")
+    phone_number = forms.CharField(max_length=800, label="Phone Numbers", help_text=" Enter Phone Numbers Seperated by ' , ' ")
+    message_body = forms.CharField(max_length=300, label="Compose Message", widget=forms.Textarea, help_text="Enter Approved Official Email")
